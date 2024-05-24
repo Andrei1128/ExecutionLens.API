3bd605377227b1e1e8e8dbd9772eaa45c175a708
Andrei-Ionuţ Nicolaev <andrei.nicolaev@365.univ-ovidius.ro>
2024-05-24 20:47:19 +0300

    fixed date

--- ExecutionLens.Domain/Extensions/DateExtensions.cs
+++ ExecutionLens.Domain/Extensions/DateExtensions.cs
@@ -4,6 +4,6 @@ public static class DateExtensions
 {
     public static DateTime NormalizeTime(this DateTime dateTime, int hours, int minutes, int seconds)
     {
-        return new DateTime(dateTime.Year, dateTime.Month, dateTime.Day + 1, hours, minutes, seconds);
+        return new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, hours, minutes, seconds);
     }
 }
